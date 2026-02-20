885bc2aeae82ea7deb63507948a5287bc97b264a
fezcode <samil.bulbul@gmail.com>
2026-02-20 23:35:31 +0300

    feat: add version flag support

--- main.go
+++ main.go
@@ -841,7 +841,14 @@ func (m model) View() string {
 	)
 }
 
+var Version = "dev"
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Printf("atlas.cam v%s\n", Version)
+		return
+	}
+
 	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error: %v\n", err)
